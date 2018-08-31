b07a49c506538ce91c45091fc702a57f043b0689
Mahfuz Shazol <6895mahfuz@gmail.com>
2018-08-31 15:33:13 +0600

    Async Controller return values

---
+++ DatingApp/Controllers/TestValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Context;
+using DatingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class TestValuesController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+
+
+        public TestValuesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<TestValues>> Index()
+        {
+            var values = await _context.TestValueses.ToListAsync();
+            return values;
+        }
+
+
+
+
+
+
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+
+            if (disposing)
+            {
+                _context.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
